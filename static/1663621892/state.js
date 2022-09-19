fdb3fdf3d23bf68141eef2d3ddc346989e6be3e2
sah-bah-hub <sashasbozhenko@gmail.com>
2022-09-20 00:24:51 +0300

    0beb2a9

---
+++ static/1663621892/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fsah-bah-hub.github.io\u002FMailConstructor\u002Fstatic\u002F1663621892",layout:"default",error:null,serverRendered:true,routePath:a,config:{_app:{basePath:"\u002FMailConstructor\u002F",assetsPath:a,cdnURL:"https:\u002F\u002Fsah-bah-hub.github.io\u002FMailConstructor\u002F"}}}}("\u002F"));
